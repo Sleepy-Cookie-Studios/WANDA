9ddf2568c60a244d32740529b8c8bd1a974e1e98
geotsam <geotsam@gmail.com>
2018-05-08 00:45:36 +0300

    Added rick&morty skill and current weather skill

--- wanda.py
+++ wanda.py
@@ -72,6 +72,13 @@ def jarvis(data):
     elif "what's your name" in data:
         speak("I'm Wanda, your Wicked Artificial Naughty Deranged Assistant.")
 
+    elif "wubba lubba dub dub" in data:
+        speak("Are you in pain? Do you want me to let you out?")
+
+    elif "what's the weather in" in data:
+        data = data.split("in")
+        location = data[1]
+        speak(weather(location))
 
     elif data != [None]:
         speak("Hmm, let me search that for you.")
@@ -103,6 +110,24 @@ def knowledgegraph(query):
     except:
         text = "I couldn't find any information."
         return text
+
+def weather(location):
+    service_url = "http://api.openweathermap.org/data/2.5/weather"
+    API_KEY = "9dd41fb2c6345abf5cd3985cdc5951fc"
+
+    params = {
+        'q':location,
+        'units':"metric",
+        'APPID':API_KEY,
+    }
+    url = service_url + '?' + urllib.urlencode(params)
+    response = json.loads(urllib.urlopen(url).read())
+    # print(json.dumps(response, indent=2))
+    weather_desc = response['weather'][0]['description']
+    weather_temp = response['main']['temp']
+    text = "The weather in " + location + " shows " + weather_desc + " and a temperature of " + str(weather_temp) + " degrees Celsius."
+    return text
+
 
 def setup():
     settings = defaultdict(int)
